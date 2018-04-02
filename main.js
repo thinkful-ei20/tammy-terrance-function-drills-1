16f5f6575853e5e59dc5fab824a4d52f77abee38
Tammy Tian <tiantammy@outlook.com>
2018-04-03 03:02:21 +0800

    added yearOfBirth function

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 'use strict';
-
+function yearOfBirth (age) {
+	return 2018 - age;
+}
 const whoAmI = function(name, age) {
-	let yearOfBirth = 2018 - age;
+	var yob = yearOfBirth(age);
 	console.log(`My name is ${name} and I'm ${age} years old.`);
-	console.log(`I was born in ${yearOfBirth}.`);
+	console.log(`I was born in ${yob}.`);
 }
 
 whoAmI('Chris', 29);
