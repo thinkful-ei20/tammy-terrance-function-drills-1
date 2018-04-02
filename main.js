4c63086282c50e69670d71bba2f07ed2904b4c8c
Terrance Corley <terrancecorley@outlook.com>
2018-04-02 11:23:36 -0700

    Added yr of birth to function

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const whoAmI = function(name, age) {
+	let yearOfBirth = 2018 - age;
 	console.log(`My name is ${name} and I'm ${age} years old.`);
+	console.log(`I was born in ${yearOfBirth}.`);
 }
 
 whoAmI('Chris', 29);
+
